57c4f22af25ea3c57825ba8fb4ae6461ca245d88
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    RMX1801: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- device.mk
+++ device.mk
@@ -162,9 +162,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
